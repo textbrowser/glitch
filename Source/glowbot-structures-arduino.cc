e278557396e6a0e0ac023bea177a8d6622817ae1
Alexis Megas <textbrowser@gmail.com>
2017-07-31 10:26:47 -0400

    Utilities.

--- Source/glowbot-structures-arduino.cc
+++ Source/glowbot-structures-arduino.cc
@@ -274,6 +274,18 @@ glowbot_structures_arduino::glowbot_structures_arduino(QWidget *parent):
       item->addChild(child);
     }
 
+  item = new QTreeWidgetItem(QStringList() << tr("Utilities"));
+  m_ui.tree->addTopLevelItem(item);
+  list.clear();
+  list << tr("PROGMEM")
+       << tr("sizeof()");
+
+  while(!list.isEmpty())
+    {
+      child = new QTreeWidgetItem(QStringList() << list.takeFirst());
+      item->addChild(child);
+    }
+
   item = new QTreeWidgetItem(QStringList() << tr("Variables"));
   m_ui.tree->addTopLevelItem(item);
   list.clear();
