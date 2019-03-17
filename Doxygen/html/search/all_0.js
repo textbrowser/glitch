04c8f9855651b5adab073fee13ff097392042d5d
textbrowser@gmail.com <textbrowser@gmail.com>
2019-03-17 14:11:21 -0400

    Doxygen.

--- Doxygen/html/search/all_0.js
+++ Doxygen/html/search/all_0.js
@@ -3,6 +3,7 @@ var searchData=
   ['glowbot_5falignment',['glowbot_alignment',['../classglowbot__alignment.html',1,'']]],
   ['glowbot_5fcanvas_5fsettings',['glowbot_canvas_settings',['../classglowbot__canvas__settings.html',1,'']]],
   ['glowbot_5fcommon',['glowbot_common',['../classglowbot__common.html',1,'']]],
+  ['glowbot_5fgraphicsview',['glowbot_graphicsview',['../classglowbot__graphicsview.html',1,'']]],
   ['glowbot_5fmisc',['glowbot_misc',['../classglowbot__misc.html',1,'']]],
   ['glowbot_5fobject',['glowbot_object',['../classglowbot__object.html',1,'']]],
   ['glowbot_5fobject_5fanalog_5fread_5farduino',['glowbot_object_analog_read_arduino',['../classglowbot__object__analog__read__arduino.html',1,'']]],
@@ -20,6 +21,7 @@ var searchData=
   ['glowbot_5ftab',['glowbot_tab',['../classglowbot__tab.html',1,'']]],
   ['glowbot_5ftab_5ftabbar',['glowbot_tab_tabbar',['../classglowbot__tab__tabbar.html',1,'']]],
   ['glowbot_5fui',['glowbot_ui',['../classglowbot__ui.html',1,'']]],
+  ['glowbot_5fundo_5fcommand',['glowbot_undo_command',['../classglowbot__undo__command.html',1,'']]],
   ['glowbot_5fuser_5ffunctions',['glowbot_user_functions',['../classglowbot__user__functions.html',1,'']]],
   ['glowbot_5fview',['glowbot_view',['../classglowbot__view.html',1,'']]],
   ['glowbot_5fview_5farduino',['glowbot_view_arduino',['../classglowbot__view__arduino.html',1,'']]]
