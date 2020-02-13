165fc1ab9ce16db3b421e9fee6c8b40d6a236936
textbrowser@gmail.com <textbrowser@gmail.com>
2020-02-12 19:50:02 -0500

    Local launch.

--- glitch.sh
+++ glitch.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-if [ -r /usr/local/glitch/Glitch ] && [ -x /usr/local/glitch/Glitch ]
+if [ -r ./Glitch ] && [ -x ./Glitch ]
+then
+    exec ./Glitch "$@"
+    exit $?
+elif [ -r /usr/local/glitch/Glitch ] && [ -x /usr/local/glitch/Glitch ]
 then
     cd /usr/local/glitch && exec ./Glitch "$@"
     exit $?
