558d32e41f0fcd1ae1e8bf6fb47a864715de971a
textbrowser@gmail.com <textbrowser@gmail.com>
2020-06-27 13:39:08 -0400

    Doxygen.

--- Doxygen/html/search/all_2.js
+++ Doxygen/html/search/all_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['private',['Private',['../classCocoaInitializer_1_1Private.html',1,'CocoaInitializer']]]
+  ['private_29',['Private',['../classCocoaInitializer_1_1Private.html',1,'CocoaInitializer']]]
 ];
