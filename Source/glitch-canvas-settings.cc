4696445cdabc1f0fef9f2721a602ec79c5889a13
textbrowser <textbrowser@gmail.com>
2022-05-02 18:45:46 -0400

    Guard SELECT().

--- Source/glitch-canvas-settings.cc
+++ Source/glitch-canvas-settings.cc
@@ -265,16 +265,18 @@ void glitch_canvas_settings::prepare(void)
       {
 	QSqlQuery query(db);
 
-	if(query.exec("SELECT "
-		      "SUBSTR(background_color, 1, 50), " // 0
-		      "SUBSTR(dots_color, 1, 50), "       // 1
-		      "SUBSTR(name, 1, 500), "            // 2
-		      "SUBSTR(output_file, 1, 5000), "    // 3
-		      "SUBSTR(project_type, 1, 50), "     // 4
-		      "redo_undo_stack_size, "            // 5
-		      "show_canvas_dots, "                // 6
-		      "SUBSTR(update_mode, 1, 100) "      // 7
-		      "FROM canvas_settings") && query.next())
+	if(query.exec(QString("SELECT "
+			      "SUBSTR(background_color, 1, 50), " // 0
+			      "SUBSTR(dots_color, 1, 50), "       // 1
+			      "SUBSTR(name, 1, %1), "             // 2
+			      "SUBSTR(output_file, 1, 5000), "    // 3
+			      "SUBSTR(project_type, 1, 50), "     // 4
+			      "redo_undo_stack_size, "            // 5
+			      "show_canvas_dots, "                // 6
+			      "SUBSTR(update_mode, 1, 100) "      // 7
+			      "FROM canvas_settings").
+		      arg(static_cast<int> (Limits::NAME_MAXIMUM_LENGTH))) &&
+	   query.next())
 	  {
 	    QColor color(query.value(0).toString().trimmed());
 	    QColor dotsColor(query.value(1).toString().trimmed());
