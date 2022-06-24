f59a12862a550b475a180ffdaa45cdf761fd795e
textbrowser <textbrowser@gmail.com>
2022-06-24 17:59:36 -0400

    Icons.

--- Source/Arduino/glitch-object-loop-arduino.cc
+++ Source/Arduino/glitch-object-loop-arduino.cc
@@ -119,6 +119,7 @@ void glitch_object_loop_arduino::addActions(QMenu &menu)
     {
       auto action = new QAction(tr("&Edit..."), this);
 
+      action->setIcon(QIcon::fromTheme("document-edit"));
       connect(action,
 	      &QAction::triggered,
 	      this,
