32e98c1b15821fc66d4986c2b8e344725d9a6a2b
textbrowser <textbrowser@gmail.com>
2022-06-27 14:11:57 -0400

    Do not allow duplicate items on a scene.

--- Source/glitch-scene.cc
+++ Source/glitch-scene.cc
@@ -202,7 +202,7 @@ bool glitch_scene::allowDrag
 
 glitch_proxy_widget *glitch_scene::addObject(glitch_object *object)
 {
-  if(!object || object->proxy())
+  if(!object || object->proxy() || object->scene() == this)
     return nullptr;
 
   auto proxy = new glitch_proxy_widget();
