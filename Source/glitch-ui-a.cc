74aac2954b521fa8368102bf1c6b2b7722962b8f
textbrowser <textbrowser@gmail.com>
2022-07-27 15:24:03 -0400

    Icons.

--- Source/glitch-ui-a.cc
+++ Source/glitch-ui-a.cc
@@ -731,12 +731,14 @@ void glitch_ui::prepareActionWidgets(void)
 
 void glitch_ui::prepareIcons(void)
 {
+  m_ui.action_About->setIcon(QIcon::fromTheme("help-about"));
   m_ui.action_Canvas_Settings->setIcon(QIcon::fromTheme("preferences-system"));
   m_ui.action_Clear_Copied_Widgets_Buffer->setIcon
     (QIcon::fromTheme("edit-clear"));
   m_ui.action_Close_Diagram->setIcon(QIcon::fromTheme("window-close"));
   m_ui.action_Copy->setIcon(QIcon::fromTheme("edit-copy"));
   m_ui.action_Delete->setIcon(QIcon::fromTheme("edit-delete"));
+  m_ui.action_Full_Screen->setIcon(QIcon::fromTheme("view-fullscreen"));
   m_ui.action_Open_Diagram->setIcon(QIcon::fromTheme("document-open"));
   m_ui.action_Paste->setIcon(QIcon::fromTheme("edit-paste"));
   m_ui.action_Quit->setIcon(QIcon::fromTheme("application-exit"));
