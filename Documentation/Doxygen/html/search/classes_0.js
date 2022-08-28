4318f004342fafd1ddc1439a38ef2c08ee014a6f
textbrowser <textbrowser@gmail.com>
2022-08-27 21:16:38 -0400

    Doxygen.

--- Documentation/Doxygen/html/search/classes_0.js
+++ Documentation/Doxygen/html/search/classes_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['cocoainitializer_50',['CocoaInitializer',['../classCocoaInitializer.html',1,'']]]
+  ['cocoainitializer_52',['CocoaInitializer',['../classCocoaInitializer.html',1,'']]]
 ];
