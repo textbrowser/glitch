f3a3a8fdc7f7ada769dcf872761266cf2d63af3e
textbrowser <textbrowser@gmail.com>
2022-09-16 19:44:01 -0400

    Doxygen.

--- Documentation/Doxygen/html/search/classes_0.js
+++ Documentation/Doxygen/html/search/classes_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['cocoainitializer_54',['CocoaInitializer',['../classCocoaInitializer.html',1,'']]]
+  ['cocoainitializer_55',['CocoaInitializer',['../classCocoaInitializer.html',1,'']]]
 ];
