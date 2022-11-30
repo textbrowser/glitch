f19790a06b0259f9220d39ff36519635e65f8de9
textbrowser <textbrowser@gmail.com>
2022-11-30 08:15:39 -0500

    Doxygen.

--- Documentation/Doxygen/html/search/classes_0.js
+++ Documentation/Doxygen/html/search/classes_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['cocoainitializer_60',['CocoaInitializer',['../classCocoaInitializer.html',1,'']]]
+  ['cocoainitializer_62',['CocoaInitializer',['../classCocoaInitializer.html',1,'']]]
 ];
--- Documentation/Doxygen/html/search/classes_2.js
+++ Documentation/Doxygen/html/search/classes_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['private_119',['Private',['../classCocoaInitializer_1_1Private.html',1,'CocoaInitializer']]]
+  ['private_123',['Private',['../classCocoaInitializer_1_1Private.html',1,'CocoaInitializer']]]
 ];
