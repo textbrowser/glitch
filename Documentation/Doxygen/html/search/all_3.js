4ee4d6006a60470c44c622203b2a5fe86afa838d
textbrowser <textbrowser@gmail.com>
2023-06-28 17:46:40 -0400

    Doxygen.

--- Documentation/Doxygen/html/search/all_3.js
+++ Documentation/Doxygen/html/search/all_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['swifty_72',['swifty',['../classswifty.html',1,'']]]
+  ['swifty_74',['swifty',['../classswifty.html',1,'']]]
 ];
--- Documentation/Doxygen/html/search/classes_3.js
+++ Documentation/Doxygen/html/search/classes_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['swifty_145',['swifty',['../classswifty.html',1,'']]]
+  ['swifty_149',['swifty',['../classswifty.html',1,'']]]
 ];
