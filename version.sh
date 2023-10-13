01ac5840f0383c1456599434e699eff418543d7e
textbrowser <textbrowser@gmail.com>
2023-10-13 19:45:30 -0400

    New version.

--- version.sh
+++ version.sh
@@ -20,4 +20,13 @@ FILE="Source/glitch-version.h"
 sed -i 's/\(GLITCH_VERSION_STRING "\)[0-9]\+\(\.[0-9]\+\)*"/\1'"$VERSION"'"/' \
     $FILE
 
+FILE="android/AndroidManifest.xml"
+
+sed -i \
+    's/\(android:versionName="\)[0-9]\+\(\.[0-9]\+\)*"/\1'"$VERSION"'"/' \
+    $FILE
+sed -i \
+    's/\(android:versionCode="\)[0-9]\+\(\.[0-9]\+\)*"/\1'"${VERSION//./}"'"/' \
+    $FILE
+
 echo "Please modify ReleaseNotes.html."
