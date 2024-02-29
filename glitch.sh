8c99ee146c9f09dc87f2f82285e33b2197804efc
textbrowser <textbrowser@gmail.com>
2024-02-29 16:13:44 -0500

    Lib does not exist.

--- glitch.sh
+++ glitch.sh
@@ -9,12 +9,6 @@ export QT_X11_NO_MITSHM=1
 if [ -r ./Glitch ] && [ -x ./Glitch ]
 then
     echo "Launching a local Glitch."
-
-    if [ -r ./Lib ]
-    then
-	export LD_LIBRARY_PATH=Lib
-    fi
-
     exec ./Glitch "$@"
     exit $?
 elif [ -r /opt/glitch/Glitch ] && [ -x /opt/glitch/Glitch ]
