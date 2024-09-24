71995c89a71f28c6a6dc08218961decfe9206dc8
textbrowser <textbrowser@gmail.com>
2024-09-24 08:46:02 -0400

    MacOS.

--- Distributions/build_macos.bash
+++ Distributions/build_macos.bash
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 # Alexis Megas.
 
+if [ ! -e glitch.pro ]; then
+    echo "Please issue $0 from the primary directory."
+    exit 1
+fi
+
 if [ ! -x ~/Qt/6.5.3/macos/bin/qmake ]; then
     echo "Please install the official Qt."
     exit 1
@@ -13,8 +18,15 @@ make -j 5
 make install
 make dmg
 
+if [ ! -r Glitch.d.dmg ]; then
+    echo "Glitch.d.dmg is not a readable file."
+    exit 1
+fi
+
 if [ "$(uname -m)" == "arm64" ]; then
     mv Glitch.d.dmg Glitch-2024.09.25_apple_silicon.d.dmg
 else
     mv Glitch.d.dmg Glitch-2024.09.25_intel.d.dmg
 fi
+
+make distclean 2>/dev/null
