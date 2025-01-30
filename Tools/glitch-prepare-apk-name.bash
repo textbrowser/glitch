b5c86aadc7e7493e71d8ec50b15d4c4f3f38603b
textbrowser <textbrowser@gmail.com>
2025-01-30 09:57:42 -0500

    New file.

---
+++ Tools/glitch-prepare-apk-name.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Alexis Megas.
+
+VERSION=$1
+
+if [ -z "$VERSION" ]
+then
+    echo "Please specify the version: $0 <VERSION>."
+    exit 1
+fi
+
+for i in $(find . -iname '*.apk')
+do
+    if [[ "$i" == *"arm64"* ]]
+    then
+	mv $i ~/Glitch-"$VERSION"_arm64.apk
+    fi
+
+    if [[ "$i" == *"armeabi"* ]]
+    then
+	mv $i ~/Glitch-"$VERSION"_arm32.apk
+    fi
+done
